637cf2204fafed293b9ba40275e6e203c591a73d
Spectr Spectrum <spectr747@gmail.com>
2019-11-08 13:15:16 +0300

    settings project shop

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include_once(ROOT . '/models/Category.php');
 
-/**
- * Description of SiteController
- *
- * @author spanch
- */
-class SiteController {
-    //put your code here
+class SiteController 
+{
+
+    public function actionIndex() 
+    {
+        $categories = array();
+        $categories = Category::getCategoriesList();
+        
+        require_once(ROOT . '/views/site/index.php');
+        
+        return true;
+    }
 }
