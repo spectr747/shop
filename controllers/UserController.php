d844eb0c4af3e4efb160a2d1c4b79d161ba6aca6
Spectr Spectrum <spectr747@gmail.com>
2019-11-10 18:40:59 +0300

    settings registration new user

---
+++ controllers/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+class UserController 
+{
+
+    public function actionRegister() 
+    {
+        $name = '';
+        $email = '';
+        $password = '';
+        
+        if (isset($_POST['submit'])) {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            
+            $errors = false;
+            
+            if (!User::checkName($name)) {
+                $errors[] = 'Имя не должно быть короче 2-х символов';
+            }
+            
+            if (!User::checkName($email)) {
+                $errors[] = 'Неправильный email';
+            }
+            
+            if (!User::checkPassword($password)) {
+                $errors[] = 'Пароль не должен быть короче 6-х символов';
+            }  
+            
+            if (User::checkEmailExists($email)) {
+                $errors[] = 'Такой email уже используется';
+            }  
+            
+            if ($errors == false) {
+                $result = User::register($name, $email, $password);
+            }
+        }
+        
+        require_once(ROOT . '/views/user/register.php');
+        
+        return true;
+    }
+    
+}
