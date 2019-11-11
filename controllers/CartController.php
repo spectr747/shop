6540018fc41e58eca1b993b7e7523e8aeb9d5993
Spectr Spectrum <spectr747@gmail.com>
2019-11-11 13:12:48 +0300

    add to card use synchronous query

---
+++ controllers/CartController.php
@@ -0,0 +1,15 @@
+<?php
+
+class CartController {
+
+    public function actionAdd($id) 
+    {
+        // Добавляем товар в корзину
+        Cart::addProduct($id);
+        
+        // Возвращаем пользователя на страницу
+        $referrer = $_SERVER['HTTP_REFERER'];
+        header("Location: $referrer");
+    }
+    
+}
